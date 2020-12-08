dd2bf5eed9160efc59ef02d8a67725476487830b
lrh2daizzo <herrera.lrh@gmail.com>
2020-12-08 10:45:47 -0600

    updated meals C + R routes

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,6 +1,7 @@
 class MealsController < ApplicationController
     get '/meals' do
         if logged_in?
+            @user = User.find(session[:user_id])
             erb :'/meals/index'
         else
             redirect to '/login'
@@ -16,6 +17,30 @@ class MealsController < ApplicationController
     end
 
     post '/meals' do
-        
+        if logged_in?
+            if params[:meal] == ""
+                redirect to '/meals/new'
+            else
+                @meal = current_user.meals.build(params[:meal])
+                if @meal.save
+                    redirect to "/meals/#{@meal.id}"
+                else
+                    redirect to "/meals/new"
+                end
+            end
+        else
+            redirect to "/login"
+        end
     end
+
+    get '/meals/:id' do
+        if logged_in?
+            @meal = Meal.find_by_id(params[:id])
+            erb :'meals/show'
+        else
+            redirect to '/login'
+        end
+    end
+
+    
 end
