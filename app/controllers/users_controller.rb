38d5f5ca7a859c0af217b7b29bedd74ff23e1d30
lrh2daizzo <herrera.lrh@gmail.com>
2020-12-07 18:52:31 -0600

    created c in CRUD routes for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,17 @@ class UsersController < ApplicationController
     end
 
     get '/signup' do
-     
+     erb :'users/new'
+    end
+
+    post '/signup' do
+        if params [:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect to '/signup'
+        else
+            @user = User.create(params[:user])
+            session[:user_id] = @user.id
+            redirect to '/meals'
+        end
     end
 
     get '/login' do
