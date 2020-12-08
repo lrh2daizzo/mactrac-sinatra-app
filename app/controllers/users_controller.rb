c56cf04b3b5443832e188d663f7ce21b8eb6dc15
lrh2daizzo <herrera.lrh@gmail.com>
2020-12-07 22:39:07 -0600

    edited routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,16 +9,40 @@ class UsersController < ApplicationController
     end
 
     post '/signup' do
-        params.inspect
         if params[:username] == "" || params[:email] == "" || params[:password] == ""
             redirect to '/signup'
         else
             @user = User.create(params[:user])
             session[:user_id] = @user.id
+            redirect `/meals`
         end
     end
 
     get '/login' do
-        
+        if !logged_in?
+            erb :'users/login'
+        else
+            redirect `/meals`
+        end
     end
+
+    post '/login' do
+        user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect to `/meals`
+        else
+            redirect to '/signup'
+        end
+    end
+
+    get '/logout' do
+        if logged_in?
+            session.destroy 
+            redirect to '/login'
+        else
+            redirect to '/'
+        end
+    end
+
 end
